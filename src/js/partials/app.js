93cb0ff09357f825cc76451d9d6ecc63650ce074
NVG\DLuzhin <DLuzhin@nvg.ru>
2017-08-30 12:16:23 +0300

    update | canvas favicon added

--- src/js/partials/app.js
+++ src/js/partials/app.js
@@ -13,3 +13,39 @@ $(document).ready(function(){
 		TweenMax.from(".animated__scale", .55, {scale:0, delay:.7, ease:Bounce.easeOut});
 	});
 });
+
+
+onload = function() {
+	cv = document.querySelector('#cvl'),
+			ctx = cv.getContext('2d');
+
+	if (!!ctx) {
+		s = 1.5 * Math.PI,
+				tc = pct = 0,
+				btn = document.querySelector('#lbtn'),
+				lnk = document.querySelector('link[rel="icon"]');
+
+		ctx.lineWidth = 2;
+		ctx.strokeStyle = 'fuchsia';
+
+		btn.addEventListener('click', function() {
+			tc = setInterval(updateLoader, 60);
+		});
+	}
+};
+
+function updateLoader() {
+	ctx.clearRect(0, 0, 16, 16);
+	ctx.beginPath();
+	ctx.arc(8, 8, 6, s, (pct * 2 * Math.PI / 100 + s));
+	ctx.stroke();
+
+	lnk.href= cv.toDataURL('image/png');
+
+	if (pct === 100) {
+		clearTimeout(tc);
+		return;
+	}
+
+	pct++;
+}
